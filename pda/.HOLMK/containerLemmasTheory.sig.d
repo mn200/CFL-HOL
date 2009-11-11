e9c2affbe90cd3404bcdfee68cc92d96ab6c2239
Michael Norrish <Michael.Norrish@nicta.com.au>
2009-11-11 01:59:30 +0000

    Remove .HOLMK directory from version control as it is generated.

--- pda/.HOLMK/containerLemmasTheory.sig.d
+++
@@ -1,1 +0,0 @@
-containerLemmasTheory.ui: /usr/local/HOL/sigobj/Thm.ui /usr/local/HOL/sigobj/term_grammar.ui /usr/local/HOL/sigobj/type_grammar.ui 
--- pda/.HOLMK/listLemmasTheory.sig.d
+++
@@ -1,1 +0,0 @@
-listLemmasTheory.ui: /usr/local/HOL/sigobj/Thm.ui /usr/local/HOL/sigobj/term_grammar.ui /usr/local/HOL/sigobj/type_grammar.ui /usr/local/HOL/sigobj/simpLib.ui 
